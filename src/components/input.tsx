484bac75029a6433b0b006324f5e9dd8b0fcd2b9
Lucas Luz <lucasluzthatsme@gmail.com>
2021-11-02 19:31:35 -0300

    add handleSubmit login

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,10 +1,10 @@
 import { Input, useColorModeValue } from '@chakra-ui/react'
 import React from 'react'
 
-export default function InputApp({ placeholder = "", isRequired = true , type = "text"}) {
+export default function InputApp({ maxLength= "" ,onChange ,placeholder = "", isRequired = true , type = "text"}) {
   const focusBorderColor = useColorModeValue("green.400", "green.200")
 
   return (
-    <Input placeholder={placeholder} isRequired={isRequired} type={type} focusBorderColor={focusBorderColor}/>
+    <Input maxLength={maxLength} onChange={(e : any) => onChange(e)} placeholder={placeholder} isRequired={isRequired} type={type} focusBorderColor={focusBorderColor}/>
   )
 }
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -13,6 +13,7 @@ import {
   InputGroup,
   InputRightElement,
   IconButton,
+
 } from '@chakra-ui/react'
 import { FaEyeSlash, FaEye } from "react-icons/fa"
 
@@ -21,10 +22,67 @@ import LoginDarkImage from '../assets/login-image-dark.svg'
 import LoginLightImage from '../assets/login-image-light.svg'
 import { ColorModeSwitcher } from '../ColorModeSwitcher'
 import InputApp from '../components/input'
+import api from '../services/api'
+import { findDOMNode } from 'react-dom'
 
 export default function Login() {
   const theme = useColorModeValue("light", "dark");
   const [showPassword, setShowPassword] = React.useState(false);
+  const [disabled, setDisabled] = React.useState(true);
+  const [error, setError] = React.useState("");
+  const [loading, setLoading] = React.useState(false);
+
+  const [cpf, setCpf] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const colorErrorText = useColorModeValue('red.400', 'red.200')
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+    try {
+      setLoading(true)
+      const response = await api.post('/login' , {cpf , password})
+      if (response.status === 201) {
+        setError('')
+        console.log(response)
+      } else {
+        setError('Erro inesperado, tente novamente em alguns minutos!')
+      }
+    } catch (e) {
+      const data = e.response.data
+      if (e.response.status === 400) {
+        if(data.rule === 'required') {
+          setError(`O campo ${data.field === 'password' ? 'Senha' :data.field.toUpperCase()} é obrigatório`)
+        }else if(data.rule === 'invalid credentials'){
+          setError(`Campo ${data.field === 'password' ? 'Senha' :data.field.toUpperCase()} inválido`)
+        }
+      }else {
+          setError('Erro inesperado, tente novamente em alguns minutos!')
+      }
+     
+    }finally {
+      setLoading(false)
+    }
+
+  }
+
+  function onChangeCpf(e: any) {
+    setCpf(e.target.value);
+    if(e.target.value !== "" && password !== "") {
+      setDisabled(false)
+    }else {
+      setDisabled(true)
+    }
+  }
+
+  function onChangePassword(e: any) {
+    setPassword(e.target.value);
+    if(e.target.value !== "" && cpf !== "") {
+      setDisabled(false)
+    }else {
+      setDisabled(true)
+    }
+  }
   return (
     <>
       <Container minH={'100vh'} minW={'100vw'} bg={useColorModeValue('gray.50', 'gray.800')} >
@@ -47,13 +105,13 @@ export default function Login() {
                 shadow="lg" spacing={6} width={"100%"} >
                 <FormControl>
                   <FormLabel>CPF</FormLabel>
-                  <InputApp placeholder="Apenas números" type="number" />
+                  <InputApp maxLength="11" onChange={onChangeCpf} placeholder="Apenas números" type="text" />
                 </FormControl>
 
                 <FormControl>
                   <FormLabel>Senha</FormLabel>
                   <InputGroup>
-                    <InputApp placeholder="Sua senha" type={showPassword === false ? 'password' : 'text'} />
+                    <InputApp onChange={onChangePassword} placeholder="Sua senha" type={showPassword === false ? 'password' : 'text'} />
                     <InputRightElement children={<IconButton
                       onClick={() => setShowPassword(!showPassword)}
                       h="1.75rem" size="sm"
@@ -63,8 +121,8 @@ export default function Login() {
                     />} />
                   </InputGroup>
                 </FormControl>
-
-                <Button width="100%" colorScheme="green">Entrar</Button>
+                {error && <Text fontSize={"17"} fontWeight={500} color={colorErrorText} alignSelf="start">{error}</Text>}
+                <Button isDisabled={disabled} type="submit" onClick={(e) => handleSubmit(e)} isLoading={loading} width="100%" colorScheme="green">Entrar</Button>
               </VStack>
             </VStack>
             <VStack width={"50%"}>
