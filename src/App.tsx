2235fc445f5075e692013c8845e9fcbb2e84fd03
Lucas Luz <lucasluzthatsme@gmail.com>
2022-04-10 10:32:40 -0300

    remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,6 @@
 import * as React from "react"
-
-import LoginImage from './assets/login-image.svg'
-import IntsLogo from './assets/ints.svg'
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  
-  VStack,
-  Code,
-  Grid,
-  Input,
-  useColorModeValue
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
-import theme from './config/theme'
 import Login from "./pages/login"
-import React from "react";
+
 
 
 export const App = () => {
